1876e9d680cef22d4766429b3176cf42b11670b2
Chandan Sharma <css08740@gmail.com>
2017-06-10 11:43:35 +0530

    Initial commit

---
+++ app/src/main/java/me/chandansharma/foodbook/FoodBookMainScreen.java
@@ -0,0 +1,13 @@
+package me.chandansharma.foodbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodBookMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_book_main_screen);
+    }
+}

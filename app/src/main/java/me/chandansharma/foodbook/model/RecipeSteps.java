92b2f7bdf649964b14e7805f877160981930bdc1
Chandan Sharma <css08740@gmail.com>
2017-06-10 12:00:15 +0530

    Implement Recipe Steps Model

---
+++ app/src/main/java/me/chandansharma/foodbook/model/RecipeSteps.java
@@ -0,0 +1,62 @@
+package me.chandansharma.foodbook.model;
+
+/**
+ * Created by iamcs on 2017-06-10.
+ * Model class for Recipe Steps
+ */
+
+public class RecipeSteps {
+
+    private String mRecipeStepsShortDescription;
+    private String mRecipeStepsDescription;
+    private String mRecipeStepsVideoUrl;
+    private String mRecipeStepsThumbnailUrl;
+
+    /**
+     * @param mRecipeStepsShortDescription Recipe Procedure Shore Description
+     * @param mRecipeStepsDescription      Recipe Procedure Long Description
+     * @param mRecipeStepsVideoUrl         Recipe Procedure Video Url
+     * @param mRecipeStepsThumbnailUrl     Recipe Procedure ThumbnailUrl
+     */
+
+
+    public RecipeSteps(String mRecipeStepsShortDescription, String mRecipeStepsDescription,
+                       String mRecipeStepsVideoUrl, String mRecipeStepsThumbnailUrl) {
+        this.mRecipeStepsShortDescription = mRecipeStepsShortDescription;
+        this.mRecipeStepsDescription = mRecipeStepsDescription;
+        this.mRecipeStepsVideoUrl = mRecipeStepsVideoUrl;
+        this.mRecipeStepsThumbnailUrl = mRecipeStepsThumbnailUrl;
+    }
+
+    public String getRecipeStepsShortDescription() {
+        return mRecipeStepsShortDescription;
+    }
+
+    public void setRecipeStepsShortDescription(String recipeStepsShortDescription) {
+        mRecipeStepsShortDescription = recipeStepsShortDescription;
+    }
+
+    public String getRecipeStepsDescription() {
+        return mRecipeStepsDescription;
+    }
+
+    public void setRecipeStepsDescription(String recipeStepsDescription) {
+        mRecipeStepsDescription = recipeStepsDescription;
+    }
+
+    public String getRecipeStepsVideoUrl() {
+        return mRecipeStepsVideoUrl;
+    }
+
+    public void setRecipeStepsVideoUrl(String recipeStepsVideoUrl) {
+        mRecipeStepsVideoUrl = recipeStepsVideoUrl;
+    }
+
+    public String getRecipeStepsThumbnailUrl() {
+        return mRecipeStepsThumbnailUrl;
+    }
+
+    public void setRecipeStepsThumbnailUrl(String recipeStepsThumbnailUrl) {
+        mRecipeStepsThumbnailUrl = recipeStepsThumbnailUrl;
+    }
+}
